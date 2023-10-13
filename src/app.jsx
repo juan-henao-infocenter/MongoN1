421fe07fa0735dcb37a9d3384cba12692907398f
Pablo.Henao <JUAN.HENAO1@infocenter.com.co>
2023-10-12 19:31:19 -0500

    Listado limpio

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProductCard from './components/ProductCard';
-
+import Header from './components/Header';
 function App() {
   const [products, setProducts] = useState([]);
-
+  const [loading, setLoading] = useState(true);
+  
   useEffect(() => {
     fetch('https://api.escuelajs.co/api/v1/products')
       .then((response) => response.json())
-      .then((data) => setProducts(data));
+      .then((data) => setProducts(data))
+      .catch((error) => console.error('Error:', error))
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
 
   return (
     <div className="container my-5">
-      <h1 className="mb-4">Lista de Productos</h1>
+      {loading ? (
+        <div className="d-flex justify-content-center align-items-center">
+        <div className="spinner-border text-primary" role="status">
+          <span className="sr-only"></span>
+        </div>
+      </div>
+      ) : (
       <div className="row">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
-      </div>
+      </div>)}
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,40 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-primary text-white text-center p-4">
-      <h1>Lista de Productos</h1>
-    </header>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+          <img
+            src="../public/logo192.png" // Coloca aquí la ruta de tu logo
+            width="30"
+            height="30"
+            alt="Logo"
+            className="d-inline-block align-top"
+          />
+          Mi Tienda
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+                Productos
+            </li>
+            <li className="nav-item">
+                Categorías
+            </li>
+          </ul>
+            <i className="fas fa-shopping-cart"></i>
+        </div>
+      </div>
+    </nav>
   );
 };
 
