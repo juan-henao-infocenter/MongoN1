b69340d606178eb8d16f164c0c955ca5d79e7051
Pablo Henao <juan.henao1@infocenter.com.co>
2023-11-05 19:26:44 -0500

    burger menu button - Validation register form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,42 +1,48 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
-const Register = () => {
-  const navigate = useNavigate();
-  const [name, setName] = useState(undefined);
-  const [email, setEmail] = useState(undefined);
-  const [password, setPassword] = useState(undefined);
-  const [confirmPassword, setConfirmPassword] = useState(undefined);
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [validated, setValidated] = useState(false);
 
-    const [validated, setValidated] = useState(false);
-  
-    const handleRegister = (event) => {
-      event.preventDefault();
-      const form = event.currentTarget;
-      console.log(form)  
-      console.log(form.checkValidity())  
-      if (!form.checkValidity()) {
-        event.stopPropagation();
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (!form.checkValidity()) {
+      e.stopPropagation();
+    }
+    setValidated(true);
+
+    if (formData.password === formData.confirmPassword) {
+      const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+      if (emailRegex.test(formData.email)) {
+        console.log("Enviando datos de registro:", formData);
       }
-  
-      setValidated(true);
-  
-      // Aquí puedes agregar tu lógica personalizada para validar los campos
-      if (form.checkValidity()) {
-        // Realiza la llamada a tu API para registrar al usuario
-        // Si la validación personalizada es exitosa
-      }
-    };
+    }
+  };
 
   return (
     <div className="container mt-5">
       <div className="row justify-content-center">
-        <div className="col-md-6">
+        <div className="col-md-8">
           <div className="card">
             <div className="card-body">
-              <h2 className="card-title text-center">Crear Cuenta</h2>
-              <form className="needs-validation" noValidate>
-                <div className="mb-3">
+              <h2 className="card-title text-center">Registro</h2>
+              <form
+                className={`row g-3 ${validated ? "was-validated" : ""}`}
+                noValidate
+                onSubmit={handleSubmit}
+              >
+                <div className="col-12">
                   <label htmlFor="name" className="form-label">
                     Nombre
                   </label>
@@ -44,31 +50,30 @@ const Register = () => {
                     type="text"
                     className="form-control"
                     id="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
+                    name="name"
+                    placeholder="Nombre"
+                    value={formData.name}
+                    onChange={handleInputChange}
                     required
                   />
-                  <div className="invalid-feedback">
-                    Por favor, ingresa tu nombre.
-                  </div>
                 </div>
-                <div className="mb-3">
+                <div className="col-12">
                   <label htmlFor="email" className="form-label">
-                    Correo
+                    Correo Electrónico
                   </label>
                   <input
                     type="email"
                     className="form-control"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    id="validationCustomEmail"
+                    placeholder="Correo electrónico"
                     required
                   />
+
                   <div className="invalid-feedback">
-                    Por favor, ingresa un correo válido.
+                    Ingresa una dirección de correo electrónico válida.
                   </div>
                 </div>
-                <div className="mb-3">
+                <div className="col-12">
                   <label htmlFor="password" className="form-label">
                     Contraseña
                   </label>
@@ -76,15 +81,14 @@ const Register = () => {
                     type="password"
                     className="form-control"
                     id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    name="password"
+                    placeholder="Contraseña"
+                    value={formData.password}
+                    onChange={handleInputChange}
                     required
                   />
-                  <div className="invalid-feedback">
-                    Por favor, ingresa una contraseña.
-                  </div>
                 </div>
-                <div className="mb-3">
+                <div className="col-12">
                   <label htmlFor="confirmPassword" className="form-label">
                     Confirmar Contraseña
                   </label>
@@ -92,21 +96,39 @@ const Register = () => {
                     type="password"
                     className="form-control"
                     id="confirmPassword"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    name="confirmPassword"
+                    placeholder="Confirmar Contraseña"
+                    value={formData.confirmPassword}
+                    onChange={handleInputChange}
                     required
+                    pattern={formData.password}
                   />
                   <div className="invalid-feedback">
-                    Por favor, confirma tu contraseña.
+                    Las contraseñas no coinciden.
                   </div>
                 </div>
-                <button
-                  type="button"
-                  className="btn btn-primary btn-block"
-                  onClick={handleRegister}
-                >
-                  Registrarse
-                </button>
+                <div className="col-12">
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="agreeCheck"
+                      required
+                    />
+                    <label className="form-check-label" htmlFor="agreeCheck">
+                      Aceptar los términos y condiciones
+                    </label>
+                    <div className="invalid-feedback">
+                      Debes aceptar los términos y condiciones.
+                    </div>
+                  </div>
+                </div>
+                <div className="col-12">
+                  <button className="btn btn-primary" type="submit">
+                    Registrarse
+                  </button>
+                </div>
               </form>
             </div>
           </div>
@@ -116,4 +138,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default RegistrationForm;
